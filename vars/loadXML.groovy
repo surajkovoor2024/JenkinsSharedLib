fb43426d40a2c412cb5773bb26d1f052e00e3a19
NJ <jainnitin2411@users.noreply.github.com>
2024-02-12 20:08:24 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -7,8 +7,9 @@ def call() {
   def xmlValue = libraryResource("paramValues.xml")
   def xml = new XmlSlurper().parseText(xmlValue)
   println("${xml.text()}")
+  println("${xml.name()}")
   // Find the ENV element based on the selected Country name
-  def selectedENV = xml.ENV.find { it.name.text() == "DEV" }
+  def selectedENV = xml.ENV.find{it.name.text() == "DEV"}
   println("Finding ENV Detail")
   println("${selectedENV.text()}")
   println("${selectedENV.PowerOption.text()}")
