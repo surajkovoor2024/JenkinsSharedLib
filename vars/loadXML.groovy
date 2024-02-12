923e0ec6c2d83c6a03726da437b6b6d66e49ca14
NJ <jainnitin2411@users.noreply.github.com>
2024-02-12 19:05:11 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -1,6 +1,4 @@
 def call() {
-  def selectedCountry = Country
-  println("Selected Country : ${Country}")
   def CountrysXmlFile = new File("//10.206.6.73/Shared/XML/paramValues.xml")
   def xmlContent = CountrysXmlFile.text
   println("XML reading Starting")
