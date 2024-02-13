2e51dd6a343265ef6f4dc12cfb2f9934db9bffe7
NJ <jainnitin2411@users.noreply.github.com>
2024-02-13 19:01:58 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -5,9 +5,11 @@ def call() {
     def list = new XmlSlurper().parseText(xmlContent)
     list.technology.name.text() // dot notation
     println list['technology']['name'].text() // square bracket notation
-
+    println 'Step1'
     def personsOver28 = list.technology.find { it.name == 'Groovy' }
+    println 'Step2'
     println XmlUtil.serialize( personsOver28 )
+    println 'Step3'
     personsOver28.each { person -> println "Name: ${person.name.text()}" }
 }
 
