77c19d1a66e0229b3245305315b13aac5fc633fd
NJ <jainnitin2411@users.noreply.github.com>
2024-02-13 18:47:57 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -1,30 +1,6 @@
 def call() {
-import groovy.xml.slurpersupport.GPathResult
-try{
-def selectedCountry = "India"
-println("Selected Country : ${selectedCountry}")
-def CountrysXmlFile = new File("//172.16.0.16/Published/ChoiceParameter/Cities.xml")
-def xmlContent = CountrysXmlFile.text
-
-println("XML reading Starting")
-// Use XMLSlurper to parse the XML content
-def xml = new XmlSlurper().parseText(xmlContent)
-
-println("XML reading worked")
-
-// Find the Country element based on the selected Country name
-def selectedCountryElement = xml.country.find { it.name.text() == selectedCountry }
-
-// Extract cities from the selected Country element
-def stateOptions = selectedCountryElement?.States.state.collect { it.name.text() }
-
-def htmlChoices = stateOptions.collect { "<option>${it}</option>" }.join('\n')
-
-} catch (Exception e) {
-    // Log the error (useful for troubleshooting)
-    println("Error reading XML file or parsing XML content: ${e.message}")
-def errorstr = e.getMessage()
-    // Return a default message or an empty dropdown in case of an error
-}
+    def list = new XmlSlurper().parseText(xml)
+    list.technology.name.text() // dot notation
+    println list['technology']['name'].text() // square bracket notation
 }
 
