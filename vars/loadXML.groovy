b4982c3b54311171f339129572da4f23ccb80ca9
NJ <jainnitin2411@users.noreply.github.com>
2024-02-13 19:12:06 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -8,7 +8,7 @@ def call() {
     println 'Step1'
     def personsOver28 = list.technology.find { it.name.text() == 'Groovy' }
     println 'Step2'
-    personsOver28.each { person -> println "Name: ${person.name}" }
+    list.technology.each { person -> println "Name: ${person.name}" }
     println 'Step3'
 }
 
