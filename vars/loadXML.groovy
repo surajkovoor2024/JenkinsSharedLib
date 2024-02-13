16b8214e7df0c528b185a2ce8c012aff3dddc2ba
NJ <jainnitin2411@users.noreply.github.com>
2024-02-13 18:28:04 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -1,18 +1,24 @@
 def call() {
   def CountrysXmlFile = new File("//10.206.6.73/Shared/XML/paramValues.xml")
-  def xmlContent = CountrysXmlFile.text
+  def xmlContent = CountrysXmlFile.text()
   println("XML reading Starting")
   println("${xmlContent }")
   //def xmlValue = libraryResource("paramValues.xml")
-
-  def parser = new XmlParser()
-  def list = parser.parseText(text)
-
-  list.each { person ->
-    println "Name: ${person.env.envname.text()}, Age: ${person.env.envage.text()}"
+  def xml = new XmlSlurper().parseText(xmlContent)
+  println("${xml.env[0].envname}")
+  //println("RootName: ${xml.name()}")
+  //println("RootName: ${xml.env.envname}")  
+  // Find the ENV element based on the selected Country name
+  //assert xml.env.envname == "DEV"
+  
+  
+  def personsOver28 = xml.children().findAll { env ->
+    env.envid.toInteger() > 1
   }
+
+  println("Size: ${personsOver28.size()}")
   
-  println list.env.envname.text()
-  
-  
-}
+  personsOver28.each { person ->
+    println "Name: ${person.envname.text()}, Age: ${person.envage.text()}"
+  }
+
