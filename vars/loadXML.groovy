cc7f93276e5c2ec1c3cf42ef96a0c4149e13d012
NJ <jainnitin2411@users.noreply.github.com>
2024-02-13 18:59:43 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -1,11 +1,14 @@
 def call() {
+    import groovy.xml.*
+
     def CountrysXmlFile = new File("//10.206.6.73/Shared/XML/paramValues.xml")
-  def xmlContent = CountrysXmlFile.text
+      def xmlContent = CountrysXmlFile.text
     def list = new XmlSlurper().parseText(xmlContent)
     list.technology.name.text() // dot notation
     println list['technology']['name'].text() // square bracket notation
 
     def personsOver28 = list.technology.find { it.name == 'Groovy' }
+    println XmlUtil.serialize( personsOver28 )
     personsOver28.each { person -> println "Name: ${person.name.text()}" }
 }
 
