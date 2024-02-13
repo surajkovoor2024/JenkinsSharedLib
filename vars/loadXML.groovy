120fa57440c415e5d4ea6116e842ceefbb616f20
NJ <jainnitin2411@users.noreply.github.com>
2024-02-13 18:33:59 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -1,6 +1,7 @@
 def call() {
   def CountrysXmlFile = new File("//10.206.6.73/Shared/XML/paramValues.xml")
-  def xmlContent = CountrysXmlFile.text()
+  //def xmlContent = CountrysXmlFile.text
+  def xmlContent = xmlParser.parseText(CountrysXmlFile)
   println("XML reading Starting")
   println("${xmlContent}")
   //def xmlValue = libraryResource("paramValues.xml")
