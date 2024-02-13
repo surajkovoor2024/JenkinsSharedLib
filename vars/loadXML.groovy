28a9480ed0c361efd3a901e3d83bed80b85ca8ed
NJ <jainnitin2411@users.noreply.github.com>
2024-02-13 18:51:24 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -1,5 +1,7 @@
 def call() {
-    def list = new XmlSlurper().parseText(xml)
+    def CountrysXmlFile = new File("//10.206.6.73/Shared/XML/paramValues.xml")
+  def xmlContent = CountrysXmlFile.text
+    def list = new XmlSlurper().parseText(xmlContent)
     list.technology.name.text() // dot notation
     println list['technology']['name'].text() // square bracket notation
 }
