4f2dea2bc703560bf2dbedd31f9fa15fcb77c324
NJ <jainnitin2411@users.noreply.github.com>
2024-02-13 16:52:16 +0530

    Update loadXML.groovy

--- vars/loadXML.groovy
+++ vars/loadXML.groovy
@@ -4,31 +4,12 @@ def call() {
   println("XML reading Starting")
   println("${xmlContent }")
   //def xmlValue = libraryResource("paramValues.xml")
-  def xml = new XmlSlurper().parseText(xmlContent)
-  println("${xml.env[0].envname}")
-  //println("RootName: ${xml.name()}")
-  //println("RootName: ${xml.env.envname}")  
-  // Find the ENV element based on the selected Country name
-  //assert xml.env.envname == "DEV"
-  
-  
-  def personsOver28 = xml.children().findAll { env ->
-    env.envid.toInteger() > 1
-  }
 
-  println("Size: ${personsOver28.size()}")
-  
-  personsOver28.each { person ->
-    println "Name: ${person.envname.text()}, Age: ${person.envage.text()}"
-  }
+  def parser = new XmlParser()
+  def list = parser.parseText(text)
 
+
+  println list.env.envname.text()
   
-  //xml.env.find {it.envname.text() == abc}  
- // xml.env.find { node ->
-//      node.envname == 'book' && node.@id == '2'
-//  }
-  //def selectedENV = xml.env.find {it.envname == "DEV"}
-  //println("Finding ENV Detail: ${selectedENV.size()}")
-  //println("Finding ENV Detail: ${selectedENV.envname}")
-  //println("${selectedENV.PowerOption.text()}")
+  
 }
