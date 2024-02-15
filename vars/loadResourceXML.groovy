a9066fe682666cf3e17d679d51df0d8322ab09e2
NJ <jainnitin2411@users.noreply.github.com>
2024-02-15 12:18:18 +0530

    Update loadResourceXML.groovy
    change xml location

--- vars/loadResourceXML.groovy
+++ vars/loadResourceXML.groovy
@@ -1,22 +1,5 @@
 def call() {
-  //  def CountrysXmlFile = new File("//10.206.6.73/Shared/XML/paramValues.xml")
-  //    def xmlContent = CountrysXmlFile.text
-  //  def list = new XmlSlurper().parseText(xmlContent)
-  //  list.technology.name.text() // dot notation
-  //  println list['technology']['name'].text() // square bracket notation
-  //  println 'Step1'
-  //  def personsOver28 = list.technology.find { it.name.text() == 'Groovy' }
-  //  println 'Step2'
-  //  list.technology.each { person -> println "Name: ${person.name}" }
-  //  println 'Step3'
-  //  //println list.technology.findAll { it.name == "Groovy" }.technology.name
-
-
-  //  def xmlFile = getClass().getResourceAsStream("//10.206.6.73/Shared/XML/paramValues.xml")    
-  //  def articles = new XmlParser().parse(xmlFile)
-  //  then: "Xml is loaded properly"
-
-def CountrysXmlFile = new File("//10.206.6.73/Shared/XML/paramValues.xml")
+def CountrysXmlFile = libraryResource("paramValues.xml")
 def xmlContent = CountrysXmlFile.text
 
 println("XML reading Starting")
