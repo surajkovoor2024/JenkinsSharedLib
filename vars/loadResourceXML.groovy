e0c8b0b5ab7a76bb2d7d588e6365ac53833abb61
NJ <jainnitin2411@users.noreply.github.com>
2024-02-15 16:46:04 +0530

    try catch

--- vars/loadResourceXML.groovy
+++ vars/loadResourceXML.groovy
@@ -1,39 +1,44 @@
 def call(selectedEnv) {
     try{
 
+        if (selectedEnv == null || selectedEnv == '') { 
+            //If the condition is true print the following statement 
+            println("Empty Environment");
+            return ["Unknow Evnrionment"]
+        } else { 
+            println "Selected Environment: ${selectedEnv}"
 
+            println("XML reading Starting")
+            def xmlContent = libraryResource('paramValues.xml')
+            println xmlContent
+            // Use XMLParser to parse the XML content
+            def xml = new XmlParser().parseText(xmlContent)
 
-println "Selected Environment: ${selectedEnv}"
-
-println("XML reading Starting")
-def xmlContent = libraryResource('paramValues.xml')
-println xmlContent
-// Use XMLParser to parse the XML content
-def xml = new XmlParser().parseText(xmlContent)
-
-println("XML reading worked")
-// Find the Country element based on the selected Country name
-def selectedCountryElement = xml.article.author.firstname.collect { it.text() }
-println xml.article.author.firstname
-println selectedCountryElement
-//return selectedCountryElement
-println "Step1"
-def selectedTitle  = xml.article.find { it.env == selectedEnv }
-println "Step1.1"
-selectedTitle = xml.article.find { it.env.text() == selectedEnv }.author.firstname.text()
-println selectedTitle
-println "Step1.2"
-def selectedTitle1 = xml.article.findAll { it.env.text() == selectedEnv }
-println selectedTitle1
-println "Step2"
-selectedTitle1 = selectedTitle1.author.collect { it.env.text() }
-println selectedTitle1
-println "Step3"
-return selectedTitle1//.author.firstname.collect { it.text() }
-     }catch(Exception ex) {
-         println("Catching the exception");
-         println ex.getMessage()
-         println ex.getCause()
-         println ex.fillInStackTrace()
-      }
+            println("XML reading worked")
+            // Find the Country element based on the selected Country name
+            def selectedCountryElement = xml.article.author.firstname.collect { it.text() }
+            println xml.article.author.firstname
+            println selectedCountryElement
+            //return selectedCountryElement
+            println "Step1"
+            def selectedTitle  = xml.article.find { it.env == selectedEnv }
+            //println "Step1.1"
+            //selectedTitle = xml.article.find { it.env.text() == selectedEnv }.author.firstname.text()
+            //println selectedTitle
+            println "Step1.2"
+            def selectedTitle1 = xml.article.findAll { it.env.text() == selectedEnv }
+            println selectedTitle1
+            println "Step2"
+            selectedTitle1 = selectedTitle1.author.collect { it.env.text() }
+            println selectedTitle1
+            println "Step3"
+            return selectedTitle1//.author.firstname.collect { it.text() }
+        } 
+    }catch(Exception ex) {
+        println selectedEnv
+        println("Catching the exception");
+        println ex.getMessage()
+        println ex.getCause()
+        println ex.fillInStackTrace()
+    }
 }
