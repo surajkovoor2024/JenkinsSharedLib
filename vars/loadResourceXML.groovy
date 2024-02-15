07db287275a91b5b0812fccde7680b55275eed41
NJ <jainnitin2411@users.noreply.github.com>
2024-02-15 17:08:15 +0530

    test

--- vars/loadResourceXML.groovy
+++ vars/loadResourceXML.groovy
@@ -6,7 +6,7 @@ def call(selectedEnv) {
             println("Empty Environment");
             return ["Unknown Environment from Git"]
         } else { 
-            println "Selected Environment: ${selectedEnv.value}"
+            println "Selected Environment: ${selectedEnv}"
 
             println("XML reading Starting")
             def xmlContent = libraryResource('paramValues.xml')
