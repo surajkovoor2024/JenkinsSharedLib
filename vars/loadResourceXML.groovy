a3d3390514bb209be6b14286513dbee7897dd88e
NJ <jainnitin2411@users.noreply.github.com>
2024-02-16 11:41:56 +0530

    Update loadResourceXML.groovy

--- vars/loadResourceXML.groovy
+++ vars/loadResourceXML.groovy
@@ -1,6 +1,6 @@
 def call(selectedEnv) {
     try{
-
+        println selectedEnv
         if (selectedEnv == null || selectedEnv == '') { 
             //If the condition is true print the following statement 
             println("Empty Environment");
