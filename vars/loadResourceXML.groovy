4b785534bc933c8412699d49c478caa7d370a565
NJ <jainnitin2411@users.noreply.github.com>
2024-02-16 12:16:57 +0530

    Update loadResourceXML.groovy

--- vars/loadResourceXML.groovy
+++ vars/loadResourceXML.groovy
@@ -1,12 +1,12 @@
-def call(Map selectedEnv = [:]) {
+def call(String selectedEnv) {
     try{
-        bat "echo Hello ${selectedEnv.name}. Today is ${selectedEnv.dayOfWeek}."
-        def abc = selectedEnv.name
-        println abc
-        println ("Method Start: ${selectedEnv.name}")
-        println ("Method Start: ${selectedEnv.dayOfWeek}")
+        //bat "echo Hello ${selectedEnv.name}. Today is ${selectedEnv.dayOfWeek}."
+        //def abc = selectedEnv.name
+        //println abc
+        println ("Method Start: ${selectedEnv}")
+        //println ("Method Start: ${selectedEnv.dayOfWeek}")
         return ["Unknown Environment from Git"]
-        if (selectedEnv == null || selectedEnv == '' || selectedEnv.name == null ||  selectedEnv.name == '') { 
+        if (selectedEnv == null || selectedEnv == '' || selectedEnv == null ||  selectedEnv == '') { 
             //If the condition is true print the following statement 
             println("Empty Environment");
             return ["Unknown Environment from Git"]
