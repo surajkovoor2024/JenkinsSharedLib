938d8f931116a8e129aa77bd2bd14b3dd25b2d72
NJ <jainnitin2411@users.noreply.github.com>
2024-02-16 12:27:50 +0530

    Update loadResourceXML.groovy

--- vars/loadResourceXML.groovy
+++ vars/loadResourceXML.groovy
@@ -36,11 +36,11 @@ def call(String selectedEnv) {
             println xml.article.author.firstname
             println selectedCountryElement
             //return selectedCountryElement
-            println "Step1"
-            def selectedTitle  = xml.article.find { it.env == selectedEnv }
-            println "Step1.1"
+            //println "Step1"
+            //def selectedTitle  = xml.article.find { it.env == selectedEnv }
+            //println "Step1.1"
             //selectedTitle = xml.article.find { it.env.text() == selectedEnv }.author.firstname.text()
-            println selectedTitle
+            //println selectedTitle
             println "Step1.2"
             def selectedTitle1 = xml.article.findAll { it.env.text() == selectedEnv }
             println selectedTitle1
