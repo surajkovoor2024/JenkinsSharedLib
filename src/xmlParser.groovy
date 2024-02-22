260ce3c45c92baf63962a6eeb6dcadac130167e3
NJ <jainnitin2411@users.noreply.github.com>
2024-02-22 17:06:45 +0530

    Update xmlParser.groovy

--- src/xmlParser.groovy
+++ src/xmlParser.groovy
@@ -3,37 +3,37 @@ class xmlParser {
     
     String getEnvIP(String selectedEnv) {
            try{
-                println ("Method Start: ${selectedEnv}")
+                println ("getEnvIP-Method Start: ${selectedEnv}")
                 if (selectedEnv == null || selectedEnv == '' || selectedEnv == null ||  selectedEnv == '') { 
                     //If the condition is true print the following statement 
-                    println("Empty Environment");
-                    return ["Unknown Environment from Git"]
+                    println("getEnvIP-Empty Environment");
+                    return ["getEnvIP-Unknown Environment from Git"]
                 } else { 
-                    println ("Selected Environment ${selectedEnv}")
-                    println("XML reading Starting")
+                    println ("getEnvIP-Selected Environment ${selectedEnv}")
+                    println("getEnvIP-XML reading Starting")
                     def xmlContent = libraryResource('vSphereEnvWithIP.xml')
                     println xmlContent
                     // Use XMLParser to parse the XML content
                     def xml = new XmlParser().parseText(xmlContent)
         
-                    println("XML reading worked")
-                    println "Step1.2"
+                    println("getEnvIP-XML reading worked")
+                    println "getEnvIP-Step1.2"
                     def envIP = xml.environments.findAll { it.environment.text() == selectedEnv }
                     println envIP
-                    println "Step2"
+                    println "getEnvIP-Step2"
                     envIP = envIP.environment.find { it.ipaddr.text() }
                     println envIP
-                    println "Step3"
+                    println "getEnvIP-Step3"
                     return envIP
                 } 
           }catch(Exception ex) {
-                    println "Error Block"
+                    println "getEnvIP-Error Block"
                     println selectedEnv
-                    println("Catching the exception");
+                    println("getEnvIP-Catching the exception");
                     println ex.getMessage()
                     println ex.getCause()
                     println ex.fillInStackTrace()
-                    return ["Unknown Environment from git ex"]
+                    return ["getEnvIP-Unknown Environment from git ex"]
           }
     }
 }
