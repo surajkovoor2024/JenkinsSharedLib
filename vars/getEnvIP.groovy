8a96fbad204e74207ec295990492662b52a9ae00
NJ <jainnitin2411@users.noreply.github.com>
2024-02-22 12:26:17 +0530

    Update getEnvIP.groovy

--- vars/getEnvIP.groovy
+++ vars/getEnvIP.groovy
@@ -1,26 +1,7 @@
 def call(String selectedEnv) {
     try{
-        println ("Method Start: ${selectedEnv}")
-        if (selectedEnv == null || selectedEnv == '' || selectedEnv == null ||  selectedEnv == '') { 
-            println("Empty Environment");
-            return ["Unknown Environment from Git"]
-        } else { 
-            println ("Selected Environment ${selectedEnv}")
-            println("XML reading Starting")
-            def xmlContent = libraryResource('paramValues.xml')
-            println xmlContent
-            // Use XMLParser to parse the XML content
-            def xml = new XmlParser().parseText(xmlContent)
-            println("XML reading worked")
-            println "Step1.2"
-            def selectedTitle1 = xml.article.findAll { it.env.text() == selectedEnv }
-            println selectedTitle1
-            println "Step2"
-            selectedTitle1 = selectedTitle1.author.collect { it.firstname.text() }
-            println selectedTitle1
-            println "Step3"
-            return selectedTitle1//.author.firstname.collect { it.text() }
-        }
+        String envIP = xmlparser.xmlParser.getEnvIP(selectedEnv)
+        echo envIP
     } catch(Exception ex) {
         println "Error Block"
         println selectedEnv
