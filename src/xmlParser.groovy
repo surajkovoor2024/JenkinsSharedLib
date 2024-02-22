024263cc8d693dba04bdd6b2d498ebaee0d6cace
NJ <jainnitin2411@users.noreply.github.com>
2024-02-22 17:29:01 +0530

    Update xmlParser.groovy

--- src/xmlParser.groovy
+++ src/xmlParser.groovy
@@ -7,8 +7,8 @@ class xmlParser {
                     return ["xmlParser-getEnvIP-If block from Git"]
                 } else {
                     def xmlContent = libraryResource('vSphereEnvWithIP.xml')
-                    def xml = new XmlParser().parseText(xmlContent)
-                    return ["xmlParser-getEnvIP-Else block from Git ${xml}"]
+                    //def xml = new XmlParser().parseText(xmlContent)
+                    return ["xmlParser-getEnvIP-Else block from Git ${xmlContent}"]
                 }               
           }catch(Exception ex) {
                     println "getEnvIP-Error Block"
