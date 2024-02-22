62e43a421a5ec720ff89c8e1969638e04cf9ab3b
NJ <jainnitin2411@users.noreply.github.com>
2024-02-22 18:31:41 +0530

    Update xmlParser.groovy

--- src/xmlParser.groovy
+++ src/xmlParser.groovy
@@ -2,22 +2,27 @@
 class xmlParser {
     
     String getEnvIP(String selectedEnv) {
+        def step = "getEnvIP-Start"
            try{
                 if (selectedEnv == null || selectedEnv == '') { 
                     return ["xmlParser-getEnvIP-If block from Git"]
                 } else {
+                    step = "getEnvIP-xml reading starting"
                     def xmlContent = libraryResource('vSphereEnvWithIP.xml')
-                    //def xml = new XmlParser().parseText(xmlContent)
+                    step = "getEnvIP-xml reading completed"
+                    def xml = new XmlParser().parseText(xmlContent)
+                    step = "getEnvIP-xml parsing completed"
                     return ["xmlParser-getEnvIP-Else block from Git ${xmlContent}"]
                 }               
-          }catch(Exception ex) {
+          } catch(Exception ex) {
+                    step = step + " " + ex.getMessage()
                     println "getEnvIP-Error Block"
                     println selectedEnv
                     println("getEnvIP-Catching the exception");
                     println ex.getMessage()
                     println ex.getCause()
                     println ex.fillInStackTrace()
-                    return ["getEnvIP-Unknown Environment from git ex"]
+                    return ["getEnvIP-Unknown Environment from git ex ${step}"]
           }
     }
 }
