ccd4b195942a83b90daae96612abfb4aa94b9e37
NJ <jainnitin2411@users.noreply.github.com>
2024-02-22 17:24:43 +0530

    Update xmlParser.groovy

--- src/xmlParser.groovy
+++ src/xmlParser.groovy
@@ -3,18 +3,10 @@ class xmlParser {
     
     String getEnvIP(String selectedEnv) {
            try{
-                println "getEnvIP-Method Start"
-                println "getEnvIP Git called-1"
-                println("Hello getEnvIP")
-                echo "Hello getEnvIP echo"
-                if (selectedEnv == null || selectedEnv == '' || selectedEnv == null ||  selectedEnv == '') { 
-                    //If the condition is true print the following statement 
-                    println("getEnvIP-Empty Environment");
-                    echo "If Condition"
-                    return ["xmlParser-getEnvIP-Unknown Environment from Git"]
+                if (selectedEnv == null || selectedEnv == '') { 
+                    return ["xmlParser-getEnvIP-If block from Git"]
                 } else { 
-                    return ["xmlParser-getEnvIP-Else block Environment from Git"]
-                    echo "else Condition"
+                    return ["xmlParser-getEnvIP-Else block from Git"]
                 }               
           }catch(Exception ex) {
                     println "getEnvIP-Error Block"
