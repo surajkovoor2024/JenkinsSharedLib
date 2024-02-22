5091902fbfe1e610a2b2affbf8106a1f1b54472b
NJ <jainnitin2411@users.noreply.github.com>
2024-02-22 12:24:33 +0530

    Update xmlParser.groovy

--- src/xmlParser.groovy
+++ src/xmlParser.groovy
@@ -1,1 +1,40 @@
 
+package xmlparser
+
+class xmlParser {
+    static String getEnvIP(String selectedEnv) {
+           try{
+                println ("Method Start: ${selectedEnv}")
+                if (selectedEnv == null || selectedEnv == '' || selectedEnv == null ||  selectedEnv == '') { 
+                    //If the condition is true print the following statement 
+                    println("Empty Environment");
+                    return ["Unknown Environment from Git"]
+                } else { 
+                    println ("Selected Environment ${selectedEnv}")
+                    println("XML reading Starting")
+                    def xmlContent = libraryResource('vSphereEnvWithIP.xml')
+                    println xmlContent
+                    // Use XMLParser to parse the XML content
+                    def xml = new XmlParser().parseText(xmlContent)
+        
+                    println("XML reading worked")
+                    println "Step1.2"
+                    def envIP = xml.environments.findAll { it.environment.text() == selectedEnv }
+                    println envIP
+                    println "Step2"
+                    envIP = envIP.environment.find { it.ipaddr.text() }
+                    println envIP
+                    println "Step3"
+                    return envIP
+                } 
+          }catch(Exception ex) {
+                    println "Error Block"
+                    println selectedEnv
+                    println("Catching the exception");
+                    println ex.getMessage()
+                    println ex.getCause()
+                    println ex.fillInStackTrace()
+                    return ["Unknown Environment from git ex"]
+          }
+    }
+}
