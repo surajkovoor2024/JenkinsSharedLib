b13d27841d03e4cee09407be75e66a6fec4328ef
NJ <jainnitin2411@users.noreply.github.com>
2024-02-26 19:29:33 +0530

    Update getEnvironment.groovy

--- vars/getEnvironment.groovy
+++ vars/getEnvironment.groovy
@@ -2,16 +2,16 @@ def call() {
   def step = 'getEnvironment-Start'
   println step
   try {
-      //step = "Environment XML reading starting"
+      step = "Environment XML reading starting"
       //println(step)
       def xmlContent = libraryResource('vSphereEnvWithIP.xml')
-      //step = "Environment XML loaded"
+      step = "Environment XML loaded"
       //println(step)
       def environments = new XmlParser().parseText(xmlContent)
-      //step = "Environment XML parsed"
+      step = "Environment XML parsed"
       //println(step)
       def selectedEnvElement = environments.environment.name.collect { it.text() }
-      //step = "Environment name collected"
+      step = "Environment name collected"
       //println(step)
       //println selectedEnvElement
       return selectedEnvElement
